b48e6bd49399cb72e88585e8a1d22b27f5b973fa
lizassel <102768939+lizassel@users.noreply.github.com>
2022-04-06 22:18:49 -0700

    Assignment 1 Submitted

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int lenght;
+
+    int width;
+
+    double area;
+
+    cout << "Enter lenght";
+    cin >> lenght;
+
+    cout << "Enter width";
+    cin >> width;
+
+    area = lenght*width;
+    cout << "The area of the rectangle";
+    cout << area;
+    return 0;
+}
